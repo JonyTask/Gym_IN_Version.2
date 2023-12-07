64b98a735aff8837d72e58620421f88bd77777cc
JonyTask <yuusu.hyb@gmail.com>
2023-12-07 19:51:29 +0900

    ルーティングの整理

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\MessageController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,18 +16,12 @@ use App\Http\Controllers\MessageController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/base', [UserController::class, 'index']);
+    Route::post('/base/edit',[UserController::class,'profileEdit']);
+    Route::post('/base/message',[MessageController::class,'SendMessage']);
+    Route::get('base/search',[UserController::class, 'Search']);
+    Route::get('base/profile',[UserController::class, 'Profile']);
+    Route::post('/base/setGym',[MessageController::class,'setGym']);
 });
-
-Route::get('/base', [UserController::class, 'index']);
-
-Route::post('/base/edit',[UserController::class,'profileEdit']);
-
-Route::post('/base/message',[MessageController::class,'SendMessage']);
-
-Route::get('base/search',[UserController::class, 'Search']);
-
-Route::get('base/profile',[UserController::class, 'Profile']);
-
-Route::post('/base/setGym',[MessageController::class,'setGym']);
